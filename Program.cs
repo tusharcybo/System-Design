5340a6892d80305cea86bbaeded252bdc7b7b46c
tusharcybo <61543686+tusharcybo@users.noreply.github.com>
2024-10-22 02:51:17 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class Journal
+    {
+        private readonly List<string> entries = new List<string>();
+        public static int count = 0;
+
+        public int AddEntry(string text)
+        {
+            entries.Add($"{++count}: {text}");
+            return count;
+        }
+
+        public void RemoveEntry(int index)
+        {
+            entries.RemoveAt(index);
+        }
+
+        public override string ToString()
+        {
+            return string.Join(Environment.NewLine, entries.ToArray());
+        }
+    }
+
+    public class Persistence
+    {
+        public void SaveToFile(Journal j, string filename, bool overwrite = false)
+        {
+            if (overwrite || !File.Exists(filename))
+            {
+                File.WriteAllText(filename, j.ToString());
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var j = new Journal();
+            j.AddEntry("I cried Today.");
+            j.AddEntry("I ate a Bug.");
+            Console.WriteLine(j);
+
+            var filename = @"C:/temp/journal.txt";
+            Persistence p = new Persistence();
+            p.SaveToFile(j, filename, true);
+            Process.Start(filename);
+            Console.ReadLine();
+        }
+    }
+
+}
